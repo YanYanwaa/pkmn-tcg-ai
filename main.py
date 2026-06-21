79fdb50d3f2f48c734b719a7aa76520475c51956
Alex Hounsell <alexander.hounsell@outlook.com>
2026-06-21 23:01:32 +0100

    Implement Greedy and Random agents

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import os
 import random
 from sdk.api import Observation, to_observation_class
+from sdk.api import OptionType
+
+BASE_SCORES = {
+    OptionType.ATTACK: 70,
+    OptionType.ABILITY: 80,
+    OptionType.EVOLVE:90,
+    OptionType.ATTACH:100,
+    OptionType.PLAY: 110,
+    OptionType.END: -50
+}
 
 def read_deck_csv() -> list[int]:
     """Read deck.csv.
@@ -18,14 +28,40 @@ def read_deck_csv() -> list[int]:
         deck.append(int(csv[i]))
     return deck
 
-def agent(obs_dict):
+def score_option(option):
+    return BASE_SCORES.get(option.type,0)
+
+
+
+def agent1(obs_dict):
 
     obs: Observation = to_observation_class(obs_dict)
 
     if obs.select is None:
         return read_deck_csv()
 
+    options = obs.select.option
+    max_count = obs.select.maxCount
+
+    scored_options = [
+        (score_option(opt), i)
+        for i, opt in enumerate(options)
+    ]
+
+    scored_options.sort(reverse=True, key=lambda x: x[0])
+
+    chosen = [i for _, i in scored_options[:max_count]]
+
+    return chosen
+
+def agent2(obs_dict):
+
+    obs: Observation = to_observation_class(obs_dict)
+
+    if obs.select is None:
+        return read_deck_csv()
+    
     return random.sample(
-        list(range(len(obs_dict["select"]["option"]))),
-        obs_dict["select"]["maxCount"]
-    )
+    list(range(len(obs_dict["select"]["option"]))),
+    obs_dict["select"]["maxCount"]
+)
--- runner.py
+++ runner.py
@@ -1,12 +1,16 @@
 from kaggle_environments import make
-from main import agent
+from main import agent1,agent2
+
 
 with open("decks/deck1.csv") as f:
     deck = [int(line) for line in f.readlines() if line.strip()]
 
 env = make("cabt", configuration={"decks": [deck, deck]})
-env.run([agent, agent])
+env.run([agent1, agent2])
 
+for i, state in enumerate(env.state):
+    print(f"Agent {i}: reward={state['reward']}, status={state['status']}")
+    
 html = env.render(mode="html")
 
 with open("result.html", "w", encoding="utf-8") as f:
